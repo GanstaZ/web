c01154df384483cded3b9b9c518496f9c9e0e0d1
GanstaZ <ganstaz@hotmail.com>
2018-10-21 03:02:21 +0300

    update and fix loading of specific blocks

--- core/block/block_manager.php
+++ core/block/block_manager.php
@@ -17,35 +17,45 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 */
 class block_manager
 {
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \dls\web\core\block\block_helper */
+	protected $block_helper;
+
 	/** @var array Contains enabled block services */
 	protected $blocks;
 
 	/**
 	* Constructor
 	*
-	* @param \ContainerInterface $container A container
+	* @param \phpbb\config\config			  $config Config object
+	* @param \dls\web\core\block\block_helper $block_helper Data helper object
+	* @param \phpbb\di\service_collection	  $blocks_data Service container
 	*/
-	public function __construct(ContainerInterface $container)
+	public function __construct(\phpbb\config\config $config, \dls\web\core\block\block_helper $block_helper, \phpbb\di\service_collection $blocks_collection)
 	{
-		$this->register_blocks($container);
+		$this->config = $config;
+		$this->block_helper = $block_helper;
+
+		$this->register_blocks($blocks_collection);
 	}
 
 	/**
 	* Register all available blocks
 	*
-	* @param ContainerInterface
+	* @param Service collection of blocks
 	* @return null
 	*/
-	protected function register_blocks($container)
+	protected function register_blocks($blocks_collection)
 	{
-		foreach ($container->get('dls.web.blocks.collection') as $block)
+		foreach ($blocks_collection as $block)
 		{
-			$block->set_config($container->get('config'));
+			$block->set_config($this->config);
 			$data = $block->get_data();
 
 			// Validate services
-			//if ($container->get('dls.web.blocks.provaider')->is_valid_service($data))
-			if ($container->get('dls.web.blocks.data_helper')->is_enabled($data['block_name']))
+			if ($this->block_helper->is_valid_data($data))
 			{
 				$this->blocks[$data['cat_name']][$data['block_name']] = $block;
 			}
@@ -53,15 +63,37 @@ class block_manager
 	}
 
 	/**
-	* Load blocks
+	* Set helper data
 	*
-	* @param string|null $cat_name
-	* @param array|null	 $block_name
+	* @param string $cat_name
 	* @return null
 	*/
-	public function load($cat_name = null, $block_name = null)
+	protected function set_helper_data($cat_name)
 	{
-		if ($blocks = $this->get_blocks($cat_name, $block_name))
+		return $this->block_helper->set($cat_name, array_flip(array_keys($this->blocks[$cat_name])));
+	}
+
+	/**
+	* Get block data
+	*
+	* @param string $cat_name
+	* @return array
+	*/
+	protected function get_block($cat_name)
+	{
+		return $this->blocks[$cat_name];
+	}
+
+	/**
+	* Load blocks
+	*
+	* @param null|mixed $cat_name
+	* @param null|array $blocks_ary Array of block names
+	* @return null
+	*/
+	public function load($cat_name = null, $blocks_ary = null)
+	{
+		if ($blocks = $this->get_blocks($cat_name, $blocks_ary))
 		{
 			$this->loading($blocks);
 		}
@@ -70,56 +102,97 @@ class block_manager
 	/**
 	* Get blocks
 	*
-	* @param string|null $cat_name
-	* @param array|null	 $block_name
+	* @param null|mixed $cat_name
+	* @param null|array $blocks_ary Array of block names
 	* @return array
 	*/
-	public function get_blocks($cat_name = null, $block_name = null)
+	public function get_blocks($cat_name = null, $blocks_ary = null)
 	{
 		if (null !== $cat_name)
 		{
-			if (!$this->blocks[$cat_name])
+			if (is_array($cat_name))
 			{
-				return [];
+				return array_filter($this->get_requested_categories($cat_name));
 			}
 
-			return $this->blocks[$cat_name];
+			$this->set_helper_data($cat_name);
+
+			return $this->get_block($cat_name);
 		}
 
-		return array_filter($this->get_all_requested_blocks($block_name));
+		return array_filter($this->get_all_blocks($blocks_ary));
 	}
 
 	/**
-	* Get all requested blocks
+	* Get requested categories
 	*
-	* @param null|array $block_name
+	* @param array $categories Array of category names
 	* @return array
 	*/
-	public function get_all_requested_blocks($block_name = null)
+	protected function get_requested_categories($categories)
 	{
-		$load_all = [];
-		foreach (array_keys($this->blocks) as $service_name)
+		$requested = [];
+		foreach ($categories as $cat_name)
 		{
-			$load_all = array_merge($load_all, $this->blocks[$service_name]);
-		}
-
-		if (is_array($block_name))
-		{
-			foreach ($block_name as $block)
+			if ($this->get_block($cat_name))
 			{
-				$array[] = $load_all[$block];
+				$this->set_helper_data($cat_name);
+				$requested = array_merge($requested, $this->get_block($cat_name));
 			}
-
-			return $array;
 		}
 
-		return $load_all;
+		return $requested;
+	}
+
+	/**
+	* Get all blocks
+	*
+	* @param null|array $blocks_ary Array of block names
+	* @return array
+	*/
+	protected function get_all_blocks($blocks_ary = null)
+	{
+		$all = $requested = [];
+		foreach (array_keys($this->blocks) as $cat_name)
+		{
+			$all = array_merge($all, $this->get_block($cat_name));
+
+			if (is_array($blocks_ary))
+			{
+				$requested = array_merge($requested, $this->get_requested_blocks($cat_name, $blocks_ary));
+			}
+		}
+
+		return ($blocks_ary) ? $requested : $all;
+	}
+
+	/**
+	* Get requested blocks
+	*
+	* @param string $cat_name
+	* @param array	$blocks_ary Array of block names
+	* @return array
+	*/
+	protected function get_requested_blocks($cat_name, array $blocks_ary)
+	{
+		$array = [];
+		foreach ($blocks_ary as $block)
+		{
+			if ($this->blocks[$cat_name][$block])
+			{
+				$this->block_helper->set($cat_name, $block);
+				$array[$block] = $this->blocks[$cat_name][$block];
+			}
+		}
+
+		return $array;
 	}
 
 	/**
 	* Loading
 	*
 	* @param array $blocks Array of enabled blocks
+	* @return void
 	*/
 	protected function loading($blocks)
 	{
@@ -132,8 +205,7 @@ class block_manager
 	/**
 	* Has blocks
 	*
-	* @param string|null $cat_name
-	*
+	* @param null|string $cat_name
 	* @return bool
 	*/
 	public function has_blocks($cat_name = null)

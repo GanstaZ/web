31e76e3a3c88fade90db1d1408b3bf2c771e638a
GanstaZ <ganstaz@hotmail.com>
2021-02-26 23:31:20 +0200

    add page helper.. wip

---
+++ core/helper.php
@@ -0,0 +1,105 @@
+<?php
+/**
+*
+* DLS Web. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2021, GanstaZ, http://www.github.com/GanstaZ/
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace dls\web\core;
+
+use phpbb\cache\service as cache;
+use phpbb\db\driver\driver_interface;
+use phpbb\user;
+
+/**
+* DLS Web helper class
+*/
+class helper
+{
+	/** @var cache */
+	protected $cache;
+
+	/** @var driver_interface */
+	protected $db;
+
+	/** @var user */
+	protected $user;
+
+	/** @var page table */
+	protected $page_data;
+
+	/**
+	* Constructor
+	*
+	* @param cache			  $cache	 Cache object
+	* @param driver_interface $db		 Database object
+	* @param user			  $user		 User object
+	* @param string			  $page_data The name of the page data table
+	*/
+	public function __construct(cache $cache, driver_interface $db, user $user, $page_data)
+	{
+		$this->cache	 = $cache;
+		$this->db		 = $db;
+		$this->user		 = $user;
+		$this->page_data = $page_data;
+	}
+
+	/**
+	* Get page data for blocks loader
+	*
+	* @return void
+	*/
+	public function get_page_data()
+	{
+		$on_page = explode('/', str_replace('.php', '', $this->user->page['page_name']));
+		$page_name = $on_page[0];
+
+		if ($page_name === 'app')
+		{
+			$page_name = count($on_page) > 2 && is_numeric(end($on_page)) ? $on_page[1] : end($on_page);
+		}
+
+		$data = $this->get($page_name);
+
+		if ($data)
+		{
+			var_dump($data);
+		}
+	}
+
+	/**
+	* Get page data
+	*
+	* @param string $name Page name
+	* @return array
+	*/
+	public function get(string $name = null): array
+	{
+		//if (($pages = $this->cache->get('_dls_pages')) === false)
+		//{
+			$sql = 'SELECT name, dls_special, dls_right, dls_left, dls_middle, dls_top, dls_bottom
+					FROM ' . $this->page_data . '
+					WHERE active = 1
+					ORDER BY id';
+			$result = $this->db->sql_query($sql);
+
+			$pages = [];
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				// Filter out inactive sections & unset page name, as we don't need it in data array
+				$data = array_keys(array_filter($row));
+				unset($data[0]);
+
+				$pages[$row['name']] = $data;
+			}
+			$this->db->sql_freeresult($result);
+
+			//$this->cache->put('_dls_pages', $pages);
+		//}
+
+		return $pages[$name] ?? [];
+	}
+}
